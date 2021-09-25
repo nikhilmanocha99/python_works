10a9047a3a6cff6e478ef7d2402786010b0e446e
nikhilmanocha99 <69859112+nikhilmanocha99@users.noreply.github.com>
2021-09-25 17:50:25 +0530

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -8,17 +8,17 @@ def spaceisfree(pos):
     return board[pos]==' '
 
 def printboard(board):
-    print('     |   |   ')
-    print(' '+board[1]+'    '+board[2]+'    '+board[3])
-    print('     |   |   ')
+    print('   |   |   ')
+    print(' '+board[1]+' | '+board[2]+' | '+board[3])
+    print('   |   |   ')
     print('----------')
-    print('     |   |   ')
-    print(' '+board[4]+'    '+board[5]+'    '+board[6])
-    print('     |   |   ')
+    print('   |   |   ')
+    print(' '+board[4]+' | '+board[5]+' | '+board[6])
+    print('   |   |   ')
     print('----------')
-    print('     |   |   ')
-    print(' '+board[7]+'    '+board[8]+'    '+board[9])
-    print('     |   |   ')
+    print('   |   |   ')
+    print(' '+board[7]+' | '+board[8]+' | '+board[9])
+    print('   |   |   ')
     
 def iswinner(bo, le):
     return (bo[7]==le and bo[8]==le and bo[9] == le) or (bo[4]==le and bo[5]==le and bo[6] == le) or (bo[1]==le and bo[2]==le and bo[3] == le) or (bo[1]==le and bo[4]==le and bo[7] == le) or (bo[2]==le and bo[5]==le and bo[8] == le) or(bo[3]==le and bo[6]==le and bo[9] == le) or (bo[1]==le and bo[5]==le and bo[9] == le) or (bo[3]==le and bo[5]==le and bo[7] == le)
